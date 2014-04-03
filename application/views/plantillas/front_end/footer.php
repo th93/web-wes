a0d058aa863ab1fc4a34e5ae3b75e8ffc4e34134
tomas <tomas@P6P7H55E>
2014-04-03 12:35:26 +0200

    Version 1.2.0

--- application/views/plantillas/front_end/footer.php
+++ application/views/plantillas/front_end/footer.php
@@ -1,7 +1,8 @@
 </section>
 <footer>
     <section class="contenedor">
-        <p>Copyright © 2014. Tomás Hurtado</p>        
+        <p>Copyright © 2014. Tomás Hurtado</p>
+        <p>Tienda Oficial Levis España</p>
     </section>
 </footer>
 </body>
